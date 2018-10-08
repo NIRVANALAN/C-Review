16bfd2498132fe5cf2f65845228294bcd17da399
NIRVANA <maverickers@outlook.com>
2018-10-08 23:37:20 +0800

    create testClass and attrs

---
+++ C++Review/testClass.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+#include "testClass.h"
+#include <iostream>
+
+std::string test_class::class_name = "testClass";
+
+void test_class::work()
+{
+	std::cout << "works well" << std::endl;
+}
---
+++ C++Review/testClass.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class test_class
+{
+public:
+	static std::string class_name;
+private:
+	std::vector<int> int_vector_;
+	std::string user_string_;
+
+public:
+
+
+	std::vector<int> int_vector() const
+	{
+		return int_vector_;
+	}
+	std::string user_string() const
+	{
+		return  user_string_;
+	}
+
+	int price1() const
+	{
+		return price_;
+	}
+
+	explicit test_class(int price)
+		: price_(price)
+	{
+	}
+	void work();
+
+
+protected:
+	int price_;
+};
